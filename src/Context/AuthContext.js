1fc7f025a93f731c4439185bf33ca7eb46d467d1
SilviaRbgl <silviarbgl@gmail.com>
2022-11-10 15:56:09 +0100

    sign in and log in function

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,32 +1,37 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import {
   getAuth,
   createUserWithEmailAndPassword,
   updateProfile,
   signInWithEmailAndPassword,
+  onAuthStateChanged,
 } from "firebase/auth";
 import { auth } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
   // console.log("props >>>", props);
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true)
+  const redirectTo = useNavigate();
 
   const signUp = async (email, password, displayName) => {
     console.log("displayName, email, password", email, password, displayName);
     try {
-      const userCredentials = await createUserWithEmailAndPassword(
+      const userSignUp = await createUserWithEmailAndPassword(
         auth,
         email,
         password,
         displayName
       );
-      console.log("userCredentials>", userCredentials);
+      console.log("userCredentials>", userSignUp);
+      setUser(userSignUp);
+      redirectTo("/");
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
-      // ..
       console.log("error>", error);
     }
   };
@@ -34,8 +39,10 @@ export const AuthContextProvider = (props) => {
   const logIn = async (email, password) => {
     console.log("email, password", email, password);
     try {
-      const userLogIn = signInWithEmailAndPassword(auth, email, password)
+      const userLogIn = await signInWithEmailAndPassword(auth, email, password)
       console.log("userLogIn", userLogIn)
+      setUser(userLogIn);
+      redirectTo("/movies");
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -44,8 +51,28 @@ export const AuthContextProvider = (props) => {
     }
   }
 
+  const checkIfUserIsLogged = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log("user is logged in");
+        setUser(user);
+        setIsLoading(false);
+      } else {
+        console.log("user is NOT logged in");
+        setUser(null);
+        setIsLoading(false);
+      }
+    });
+  }
+
+  useEffect(() => {
+    checkIfUserIsLogged()
+  }, [])
+  
+
   return (
-    <AuthContext.Provider value={{ user, setUser, signUp, logIn }}>
+    <AuthContext.Provider value={{ user, setUser, signUp, logIn, }}>
       {props.children}
     </AuthContext.Provider>
   );
