a6d54be12c48609cf22d6250b6f85588966fdc6c
SilviaRbgl <silviarbgl@gmail.com>
2022-11-11 16:54:43 +0100

    view for the logged in  users

--- src/Components/DetailsMovieAuth.js
+++ src/Components/DetailsMovieAuth.js
@@ -1,20 +1,53 @@
 import React, { useContext, useEffect, useState } from "react";
 import { db } from "../config";
 import { AuthContext } from "../Context/AuthContext";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { async } from "@firebase/util";
 
 function DetailsMovieAuth() {
   const { user } = useContext(AuthContext);
+  const [reviewsMsgs, setReviewsMsgs] = useState([]);
+  const [review, setReview] = useState("");
 
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   const getReviews = async () => {
-    const querySnapshot = await getDocs(collection(db, "Reviews"));
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-    });
+    try {
+      const querySnapshot = await getDocs(collection(db, "Reviews"));
+      const myMsgs = [];
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        myMsgs.push(doc.data());
+      });
+      console.log("myMsgs>", myMsgs);
+      setReviewsMsgs(myMsgs);
+    } catch (error) {
+      console.log("error", error);
+    }
   };
+
+  const msgDate = (date) => {
+    return new Date(date*1000).toLocaleString();
+  };
+
+  const handleMessageChange = (e) => {
+    setReview(e.target.value)
+  };
+
+  const handleSubmit = async () => {
+    console.log("review", review)
+    try {
+      const docRef = await addDoc(collection(db, "Reviews"), {
+        text: review,
+        date: new Date(),
+        author: user.email
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
 
   useEffect(() => {
     getReviews();
@@ -44,22 +77,26 @@ function DetailsMovieAuth() {
       </div>
 
       <div className="Container-Comment" action="">
-        <p>Your review:</p>
-        <label htmlFor="msg"></label>
-        <input type="text" placeholder="Review here" name="msg" required />
+        <p>Review this movie:</p>
+        <input type="text" value={review} placeholder="Review here" name="message" onChange={handleMessageChange} required />
+        <label htmlFor="message"></label>
         <br></br>
-        <button type="submit" className="Button">
-          Send
+        <button className="Button" type="submit" onClick={handleSubmit} >
+          Submit review
         </button>
       </div>
-
-      <div className="Container-Comment">
-        {/* <p>Users reviews:</p>
-      <div className="grid">
-        {reviews.map((review) => {
-          return <Review key={review.id} review={review}/>;
-        })}
-      </div> */}
+      <p>Users reviews:</p>
+      <div className="container-review">
+        {reviewsMsgs &&
+          reviewsMsgs.map((msg, index) => {
+            return (
+              <div key={index}>
+                <p>{msg.author} says:</p>
+                <p>"{msg.text}"</p>
+                <p>{msgDate(msg.date.seconds)}</p>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
